3770a784199ebd4b2d41f79c573fb04de13219da
Raja <kraja200709@gmail.com>
2022-06-23 14:46:40 +0530

    init

---
+++ app/Http/Controllers/IssueCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IssueCategory;
+use Illuminate\Http\Request;
+
+class IssueCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\IssueCategory  $issueCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(IssueCategory $issueCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\IssueCategory  $issueCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(IssueCategory $issueCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\IssueCategory  $issueCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, IssueCategory $issueCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\IssueCategory  $issueCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(IssueCategory $issueCategory)
+    {
+        //
+    }
+}
