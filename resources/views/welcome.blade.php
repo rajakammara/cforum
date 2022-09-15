fa6eb055e492e427b1781c18bc879ba4a0679ca2
Raja <kraja200709@gmail.com>
2022-09-16 02:45:42 +0530

    title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>C-Forum</title>
+    <title>Aarogya Anantapuramu</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
